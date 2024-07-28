30a2a596590f028f250589063cb732df44bf6e9f
Hemant Barasker <138351690+hemantbrskr@users.noreply.github.com>
2024-07-28 23:07:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+      var typed = new Typed("#element", {
+        strings: [
+          "FrontEnd Developer",
+          "Mern Stack Developer",
+          "Problem Solver",
+          "Creative Thinker",
+          "Good at Teamwork",
+          "Time Managiable",
+          "Fast Adapter",
+        ],
+        typeSpeed: 50,
+      });
